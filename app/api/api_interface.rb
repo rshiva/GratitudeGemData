9b1407460837f4d92cf9887825d47cfa1b8997e5
rshiva <nadarshiva.r@gmail.com>
2013-07-28 21:23:49 +0530

    post request successfully creating record from curl :D ,need to validate email

--- app/api/api_interface.rb
+++ app/api/api_interface.rb
@@ -7,18 +7,21 @@ module ApiInterface
 			get  do
         GemData.all.to_json        
       end  
-
+      
+      params do
+        requires :author_email , type: String , desc: "Author email id" #,, regexp: /\A([\w\!\#\z\%\&\'\*\+\-\/\=\?\\A\`{\|\}\~]+\.)*[\w\+-]+@((((([a-z0-9]{1}[a-z0-9\-]{0,62}[a-z0-9]{1})|[a-z])\.)+[a-z]{2,6})|(\d{1,3}\.){3}\d{1,3}(\:\d{1,5})?)\z/i 
+        requires :user_email , type: String , desc: "User email id" #, regexp: /\A([\w\!\#\z\%\&\'\*\+\-\/\=\?\\A\`{\|\}\~]+\.)*[\w\+-]+@((((([a-z0-9]{1}[a-z0-9\-]{0,62}[a-z0-9]{1})|[a-z])\.)+[a-z]{2,6})|(\d{1,3}\.){3}\d{1,3}(\:\d{1,5})?)\z/i
+      end
       post do
-      params={:author_email => params[:author_email], :user_email => params[:user_email] ,gem_name: params[:gem_name]}
-      @gem_data=GemData.new(params) if params
-      @gem_data.save
-      status 201
-      @gem_data
+        post_params={:author_email => params[:author_email],:user_email => params[:user_email],:gem_name  => params[:gem_name]}
+        # didnt used params as variable name because giving error NoMethodError (undefined method `[]' for nil:NilClass):
+        @gem_data=GemData.new(post_params)
+        @gem_data.save
+        @gem_data
       end 
-      #curl -X POST "http://localhost:3001/api/data/helloup.json"
-      #curl -d '{"author_email": "r6@gmail.com" , "user_email": "shiva@example.com","gem": "has_me"}' -X POST -H Content-Type:application/json http://localhost:3001/api/data
+
 		end
-  
   end
-	
 end
+
+      #curl -d '{"author_email": "Metallica", "user_email": "metallica@metalhead.com"}' -X POST -H Content-Type:application/json h//localhost:3000/api/data.json
