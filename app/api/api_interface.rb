cfdb2546f2d86875fe6a670c54caa00015a483f2
rshiva <nadarshiva.r@gmail.com>
2013-08-15 18:35:13 +0530

    put method for api

--- app/api/api_interface.rb
+++ app/api/api_interface.rb
@@ -4,7 +4,9 @@ module ApiInterface
 	class API < Grape::API
 		
 	#version 'v1', using: :path
+  #version 'v1', using: :header , vendor: ""
     default_format :json
+   # do_not_route_options!
 
     resources :data do
      	
@@ -28,6 +30,21 @@ module ApiInterface
         @gem_data.save
         @gem_data
       end 
+
+
+      params do
+        requires :author_email , type: String , desc: "Author email id" , regexp: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i 
+        requires :user_email , type: String , desc: "User email id" , regexp: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i 
+        optional :gem_name, type: String #default: 'blue'
+      end
+
+      put ':id' do
+        @gem_data=GemData.find(params[:id])
+        @gem_data.user_email=params[:user_email] if params[:user_email]
+        @gem_data.author_email = params[:author_email] if params[:author_email]
+        @gem_data.save
+      end
+      
 
     end
   end
